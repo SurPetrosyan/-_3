dcefd1ddf042614cea33146604dd140a85c17d3e
1P <sur141097@gmail.com>
2023-04-20 23:34:12 +0300

    new commit

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,4 +1,4 @@
-from functions import filter_sort, format_date, mask_card
+from functions import filter_sort, format_date, mask_card, formatted_data, load_data
 
 
 def test_filter_sort():
@@ -41,4 +41,65 @@ def test_format_date():
 
 def test_mask_card():
     assert mask_card("Счет 75106830613657916952") == 'Счет **6952'
+    assert mask_card("Visa Classic 6831982476737658") == 'Visa Classic 6831 98** **** 7658'
 
+
+def test_formatted_data():
+    dict_1 = {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {
+            "amount": "56883.54",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229"
+    }
+    dict_2 = {
+        "id": 596171168,
+        "state": "EXECUTED",
+        "date": "2018-07-11T02:26:18.671407",
+        "operationAmount": {
+            "amount": "79931.03",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 72082042523231456215"
+    }
+    str_1 = '19.08.2018 Перевод с карты на карту\n' \
+            'Visa Classic 6831 98** **** 7658 -> Visa Platinum 8990 92** **** 5229\n' \
+            '56883.54 USD\n'
+    str_2 = '11.07.2018 Открытие вклада\n' \
+            'Счет  **6215\n' \
+            '56883.54 руб.\n'
+    assert formatted_data(dict_1) == str_1
+    assert formatted_data(dict_2) == str_2
+
+
+def test_load_data():
+    list_1 = [
+        {
+            "id": 596171168,
+            "state": "EXECUTED",
+            "date": "2018-07-11T02:26:18.671407",
+            "operationAmount": {
+                "amount": "79931.03",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 72082042523231456215"
+        }
+
+    ]
+    assert load_data() == list_1
